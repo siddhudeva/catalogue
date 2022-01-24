7138fbcedd29c8a2de180a2e63864b8bf1aebd78
siddhu <siddhudangeti@gmail.com>
2022-01-24 21:13:34 +0530

    catalogue file setup

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,3 +1,6 @@
+//// module is a container contains of all resources and can call other modules.which lets us include the child modules resources into configuration in concise way.
+////we add source link in module if it is git or any other remote repository
+//// simply mentioning module in root file is stating that all the other resources are here only.(just to keep code dry)and also we have to tell which are the varibles we are calling from that repository
 module "app-module" {
   source           = "git::https://github.com/siddhudeva/terraform_mutable_commom_infra.git"
   INSTANCES_NO     = var.INSTANCES_NO
